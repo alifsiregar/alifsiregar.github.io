77f3cb873bf27ea050b268297d5a8069d46486e0
Alif Siregar <alifsiregar21@gmail.com>
2020-10-03 15:50:13 +0700

    update informations

--- java.js
+++ java.js
@@ -1,28 +1,28 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-const teks = ["God's in His heaven", "All's right with the world"];
+const teks = ["Welcome", "Selamat Datang"];
 let i = 0;
 let j = 0;
 let currentText = "";
 let letter = "";
 
-async function type(){
-    if(i === teks.length){
-        i = 0;
-    }
-    currentText = teks[i];
-    letter = currentText.slice(0, ++j);
+async function type() {
+  if (i === teks.length) {
+    i = 0;
+  }
+  currentText = teks[i];
+  letter = currentText.slice(0, ++j);
 
-    document.querySelector(".texts").textContent = letter;
-    if(letter.length === currentText.length ){
-        await sleep(500);
-        i++;
-        j=0;
-    }
+  document.querySelector(".texts").textContent = letter;
+  if (letter.length === currentText.length) {
+    await sleep(500);
+    i++;
+    j = 0;
+  }
 
-    setTimeout(type, 200);
+  setTimeout(type, 200);
 }
 
 type();
